348b1d51313072439b1b17234eedafc54efd8a63
SriramR <srivishal600@gmail.com>
2023-07-24 17:54:34 +0530

    Initial Commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:swiggy_app/main.dart';
+import 'package:swiggy/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
